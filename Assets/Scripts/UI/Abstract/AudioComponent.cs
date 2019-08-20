fe870697786eeb3d44de34b35450dd6570950fb2
Firat Ozbay <firatozbay94@gmail.com>
2019-08-20 15:51:11 +0300

    - Added Localized Audio class

---
+++ Assets/Scripts/UI/Abstract/AudioComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public abstract class AudioComponent : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    protected virtual void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    protected void SetAudioClip(AudioClip audioClip)
+    {
+        _audioSource.clip = audioClip;
+    }
+
+    public void Play()
+    {
+        _audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/UI/LocalizedAudio.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LocalizedAudio : AudioComponent
+{
+    [SerializeField]
+    private MultiLanguageAudioClip _multiLanguageAudioClip;
+
+    private bool _initialized;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Init();
+    }
+
+    private void Init()
+    {
+        if (_initialized)
+            return;
+        _initialized = true;
+        if (_multiLanguageAudioClip != null)
+            SetAudioClip(LanguageManager.GetAudioClip(_multiLanguageAudioClip));
+
+        LanguageManager.Instance.OnLanguageChange += (language) => {
+            if (this && gameObject && _multiLanguageAudioClip != null)
+                SetMultiLanguageAudioClip(_multiLanguageAudioClip);
+        };
+    }
+
+    public void SetMultiLanguageAudioClip(MultiLanguageAudioClip audio)
+    {
+        Init();
+        _multiLanguageAudioClip = audio;
+        if (_multiLanguageAudioClip != null)
+            SetAudioClip(LanguageManager.GetAudioClip(_multiLanguageAudioClip));
+    }
+}
